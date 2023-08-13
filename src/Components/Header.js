6a9ea56377f071f0112374980620f798607699e3
Rupesh <112386480+Rupesh180902@users.noreply.github.com>
2023-08-13 12:21:00 +0530

    Header Completed

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Typed from "react-typed"
+export default function Header() {
+  return (
+    <div className='header-wraper'>
+      <div className="main-info">
+        <h1> Web Development </h1>
+        <Typed
+        className='typed-text'
+        strings={['Web Design','Web Development','Google Ads']}
+        typeSpeed={40}
+        backSpeed={60}
+        loop
+        />
+        <a href="/" className='btn-main-offer'>Contact Me</a>
+      </div>
+    </div>
+  )
+}
